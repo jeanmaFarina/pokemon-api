8a51bd5af1cd63fc0b37424513f66a1c720f4eae
jeanmarie farina <farina.jeanmarie@outlook.com>
2022-03-16 12:04:49 +0100

    added gltf loader

--- script/script.js
+++ script/script.js
@@ -1,72 +1,22 @@
+import {GLTFLoader} from "./GLTFLoader.js"
 
-const scene = new THREE.Scene();
-
-const ambientLight = new THREE.AmbientLight(0xffffff, 0.6);
-scene.add(ambientLight);
-
-const directionalLight = new THREE.DirectionalLight(0xffffff, 0.8);
-directionalLight.position.set(100, 500, 300);
-scene.add(directionalLight);
-
-// Setting up camera
-const aspectRatio = window.innerWidth / window.innerHeight;
-const cameraWidth = 150;
-const cameraHeight = cameraWidth / aspectRatio;
-
-const camera = new THREE.OrthographicCamera(
-  cameraWidth / -2, // left
-  cameraWidth / 2, // right
-  cameraHeight / 2, // top
-  cameraHeight / -2, // bottom
-  0, // near plane
-  1000 // far plane
-);
-camera.position.set(200, 200, 200);
-camera.lookAt(-13, 15, 10);
-
-// Set up renderer
-const renderer = new THREE.WebGLRenderer({ antialias: true });
-renderer.setSize(window.innerWidth, window.innerHeight);
-
-function createWheels() {
-  const geometry = new THREE.BoxBufferGeometry(12, 12, 33);
-  const material = new THREE.MeshLambertMaterial({ color: 0x007700 });
-  const wheel = new THREE.Mesh(geometry, material);
-  return wheel;
+let scene = new THREE.Scene()
+let camera = new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight,0.01,1000)
+let render = new THREE.WebGLRenderer()
+render.setSize(window.innerWidth,window.innerHeight)
+document.body.appendChild(render.domElement)
+let loader = new GLTFLoader()
+let gameboy
+loader.load("./ressources/gameboy/scene.gltf",(gltf)=>{
+  gameboy = gltf.scene
+  scene.add(gltf.scene)
+})
+let light = new THREE.HemisphereLight(0xffffff,0x000000,2)
+scene.add(light)
+camera.position.set(0,0,19)
+function animate(){
+  requestAnimationFrame(animate)
+  gameboy.rotate.y += 0.05
+  render.render(scene,camera)
 }
-function createCar() {
-  const car = new THREE.Group();
-
-  const backWheel = createWheels();
-  backWheel.position.y = 6;
-  backWheel.position.x = -18;
-  car.add(backWheel);
-
-  const frontWheel = createWheels();
-  frontWheel.position.y = 6;
-  frontWheel.position.x = 18;
-  car.add(frontWheel);
-
-  const main = new THREE.Mesh(
-    new THREE.BoxBufferGeometry(60, 15, 30),
-    new THREE.MeshLambertMaterial({ color: 0x009900 })
-  );
-  main.position.y = 12;
-  car.add(main);
-
-  const cabin = new THREE.Mesh(
-    new THREE.BoxBufferGeometry(33, 12, 24),
-    new THREE.MeshLambertMaterial({ color: 0x229900 })
-  );
-  cabin.position.x = -6;
-  cabin.position.y = 25.5;
-  car.add(cabin);
-
-  return car;
-}
-
-const car = createCar();
-scene.add(car);
-
-renderer.render(scene, camera);
-document.body.appendChild(renderer.domElement);
+animate()
