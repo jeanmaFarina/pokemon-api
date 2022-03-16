34cd4a8cf9b001325df8d3b4a84485eb9cab37d5
jeanmarie farina <farina.jeanmarie@outlook.com>
2022-03-16 15:36:36 +0100

    added controls

--- script/script.js
+++ script/script.js
@@ -1,22 +1,41 @@
 import {GLTFLoader} from "./GLTFLoader.js"
-
-const scene = new THREE.Scene();
+import {OrbitControls} from "./OrbitControls.js"
+const scene = new THREE.Scene({ antialias: true});
 scene.background = new THREE.Color(0x0F0F0F)
+
 let camera = new THREE.PerspectiveCamera(70,window.innerWidth/window.innerHeight,0.1,5000)
-camera.rotation.y = 45/180*Math.PI
+camera.rotation.y = 20/180*Math.PI
 camera.position.y = 100
-camera.position.x = 600
+camera.position.x = 500
 camera.position.z = 1000
-const ambientLight = new THREE.AmbientLight(0xffffff, 0.6);
+
+const ambientLight = new THREE.AmbientLight(0xffffff, 1.5);
 scene.add(ambientLight);
+
+const directionalLight = new THREE.DirectionalLight(0xffffff,2)
+directionalLight.position.set(0.5,4,0.5)
+directionalLight.castShadow = true
+scene.add(directionalLight)
+
 let renderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth,window.innerHeight)
+
 document.body.appendChild(renderer.domElement)
+
+const controls = new OrbitControls(camera,renderer.domElement)
+controls.addEventListener("change", renderer)
+
 let loader = new GLTFLoader()
 let gameboy
 loader.load("./ressources/gameboy/scene.gltf",(gltf)=>{
   gameboy = gltf.scene
   scene.add(gltf.scene)
-  renderer.render(scene,camera)
+  animate()
   console.log("model loaded")
 })
+
+function animate(){
+  renderer.render(scene,camera)
+  requestAnimationFrame(animate)
+
+}
